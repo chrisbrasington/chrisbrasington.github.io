14c1f9d594b07b6978dccf26a9cc9115e7f5e1db
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2025-04-06 19:31:15 -0600

    2025-04-06 19:31

--- scripts/get_date.js
+++ scripts/get_date.js
@@ -2,7 +2,8 @@ function showTime() {
 	const date = new Date();
 
 	let today = date.toLocaleString("en", { weekday: "long" });
-	let time = date.toLocaleTimeString("en", { hour12: true }); // full time string with AM/PM
+	// let time = date.toLocaleTimeString("en", { hour12: true }); // full time string with AM/PM
+	let time = date.toLocaleTimeString("en", { hour12: true, hour: '2-digit', minute: '2-digit' }); // no seconds
 	let day = date.toLocaleString("en", { day: "2-digit" });
 	let month = date.toLocaleString("en", { month: "2-digit" });
 	let year = date.toLocaleString("en", { year: "numeric" });
