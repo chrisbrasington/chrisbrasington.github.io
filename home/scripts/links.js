be0bd10071cf3eb68f99580bcbac282fb69f5bfc
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2026-04-28 21:44:43 -0600

    2026-04-28 21:44

---
+++ home/scripts/links.js
@@ -0,0 +1,47 @@
+const linkData = {
+    "~/gaming ": [
+        { "name": "Steam", "url": "https://store.steampowered.com/" },
+        { "name": "Steam/raylinth", "url": "https://steamcommunity.com/id/raylinth/home" },
+        { "name": "Twitch", "url": "https://www.twitch.tv/" },
+        { "name": "Discord", "url": "https://discord.com/login" }
+    ],
+    "~/reading 🕮": [
+        { "name": "storygraph", "url": "https://app.thestorygraph.com/" },
+        { "name": "Overdrive", "url": "https://www.overdrive.com/account/libraries" },
+        { "name": "Anna's Archive", "url": "https://annas-archive.org/", "hidden": true },
+        { "name": "z-lib", "url": "https://z-lib.gd/", "hidden": true },
+        { "name": "esnovels", "url": "https://esnovels.github.io/EsNovels1/", "hidden": true },
+        { "name": "zero translation", "url": "https://zetrotranslation.com/" },
+        { "name": "Project Gutenberg", "url": "https://www.gutenberg.org/" },
+        { "name": "ABB", "url": "https://audiobookbay.lu/", "hidden": true },
+        { "name": "audiobook4soul", "url": "https://audiobooks4soul.com/", "hidden": true}
+    ],
+    "~/dev  ⌨": [
+        { "name": "GitHub", "url": "https://github.com/chrisbrasington?tab=repositories" },
+        { "name": ".NET", "url": "https://devblogs.microsoft.com/dotnet/" },
+        { "name": "Chat-GPT", "url": "https://chat.openai.com/" }
+    ],
+    "~/linux ": [
+        { "name": "Debian Wiki", "url": "https://wiki.debian.org/" },
+        { "name": "Flathub", "url": "https://flathub.org/" },
+        { "name": "ProtonDB", "url": "https://www.protondb.com/" },
+        { "name": "valhalla", "url": "https://valhalla:8384", "hidden": true },
+        { "name": "cb-steamdeck", "url": "https://cb-steamdeck:8384", "hidden": true },
+        { "name": "cb-sapphire", "url": "https://cb-sapphire:8384", "hidden": true },
+        { "name": "cb-debian", "url": "https://cb-debian:8384", "hidden": true }
+    ],
+    "~/media 🖭": [
+        { "name": "youtube-zero", "url": "http://valhalla:8000/"},
+        { "name": "Tidal", "url": "https://listen.tidal.com/" },
+        { "name": "Last.FM", "url": "https://www.last.fm/user/raylinth" },
+        { "name": "Youtube Subs", "url": "https://www.youtube.com/feed/subscriptions" },
+        { "name": "Plex", "url": "https://app.plex.tv/desktop/#/" },
+        { "name": "HDtoday.tv", "url": "https://hdtoday.tv/home", "hidden": true },
+        { "name": "TED-Ed", "url": "https://www.youtube.com/@TEDEd" },
+        { "name": "Wallhaven", "url": "https://wallhaven.cc/" }
+    ],
+    "~/anime 萌": [
+        { "name": "Crunchyroll", "url": "https://www.crunchyroll.com/" },
+        { "name": "MangaDex", "url": "https://mangadex.org/" }
+    ]
+};
