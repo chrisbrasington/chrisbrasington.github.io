5fbce8862bae732becb8c7d3443d5eda0c628604
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2026-09-19 15:18:29 -0600

    2026-09-19 15:18

--- home/scripts/links.js
+++ home/scripts/links.js
@@ -1,33 +1,16 @@
 const linkData = {
     "~/gaming ": [
         { "name": "Steam", "url": "https://store.steampowered.com/" },
-        { "name": "Steam/raylinth", "url": "https://steamcommunity.com/id/raylinth/home" },
-        { "name": "Twitch", "url": "https://www.twitch.tv/" },
-        { "name": "Discord", "url": "https://discord.com/login" }
+        { "name": "Steam/raylinth", "url": "https://steamcommunity.com/id/raylinth/home" }
     ],
     "~/reading 🕮": [
         { "name": "storygraph", "url": "https://app.thestorygraph.com/" },
         { "name": "Overdrive", "url": "https://www.overdrive.com/account/libraries" },
-        { "name": "Anna's Archive", "url": "https://annas-archive.org/", "hidden": true },
+        { "name": "Anna's Archive", "url": "https://annas-archive.gd/", "hidden": true },
         { "name": "z-lib", "url": "https://z-lib.gd/", "hidden": true },
-        { "name": "esnovels", "url": "https://esnovels.github.io/EsNovels1/", "hidden": true },
-        { "name": "zero translation", "url": "https://zetrotranslation.com/" },
         { "name": "Project Gutenberg", "url": "https://www.gutenberg.org/" },
         { "name": "ABB", "url": "https://audiobookbay.lu/", "hidden": true },
         { "name": "audiobook4soul", "url": "https://audiobooks4soul.com/", "hidden": true}
-    ],
-    "~/dev  ⌨": [
-        { "name": "GitHub", "url": "https://github.com/chrisbrasington?tab=repositories" },
-        { "name": ".NET", "url": "https://devblogs.microsoft.com/dotnet/" }
-    ],
-    "~/linux ": [
-        { "name": "Debian Wiki", "url": "https://wiki.debian.org/" },
-        { "name": "Flathub", "url": "https://flathub.org/" },
-        { "name": "ProtonDB", "url": "https://www.protondb.com/" },
-        { "name": "valhalla", "url": "https://valhalla:8384", "hidden": true },
-        { "name": "cb-steamdeck", "url": "https://cb-steamdeck:8384", "hidden": true },
-        { "name": "cb-sapphire", "url": "https://cb-sapphire:8384", "hidden": true },
-        { "name": "cb-debian", "url": "https://cb-debian:8384", "hidden": true }
     ],
     "~/media 🖭": [
         { "name": "youtube-zero", "url": "https://youtube.home.chrisincode.com"},
@@ -40,6 +23,8 @@ const linkData = {
         { "name": "Wallhaven", "url": "https://wallhaven.cc/" }
     ],
     "~/anime 萌": [
+        { "name": "manga.home", "url": "https://manga.home.chrisincode.com/"},
+        { "name": "onepiece.home", "url": "https://onepiece.home.chrisincode.com/"},
         { "name": "Crunchyroll", "url": "https://www.crunchyroll.com/" },
         { "name": "MangaDex", "url": "https://mangadex.org/" }
     ]
